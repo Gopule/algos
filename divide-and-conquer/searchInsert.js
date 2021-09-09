6fe1285bca7d70338b3792465d9a6d92d551bcca
Gopule <altusszawlowski@gmail.com>
2021-09-09 18:28:58 -0400

    add exenstion

--- divide-and-conquer/searchInsert.js
+++ divide-and-conquer/searchInsert.js
@@ -22,4 +22,4 @@ console.log(searchInsert([1, 3, 5, 6], 2)); //1
 console.log(searchInsert([1, 3, 5, 6], 7)); //4
 console.log(searchInsert([1, 3, 5, 6], 0)); //0
 console.log(searchInsert([1], 0)); //0
-console.log(searchInsert([1, 3], 1)); //1
+console.log(searchInsert([1, 3], 1)); //0
