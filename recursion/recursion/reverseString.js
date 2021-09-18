5a5b97315554cdd29f8cd9669fad7a13c2e0b35c
Gopule <altusszawlowski@gmail.com>
2021-09-18 15:38:42 -0400

    climb stairs

--- recursion/recursion/reverseString.js
+++
@@ -1,9 +0,0 @@
-function reverse(string) {
-  if (string.length === 0) return "";
-  let lastEl = string[string.length - 1];
-  let reverseString = reverse(string.slice(0, -1));
-  return (lastEl += reverseString);
-}
-
-console.log(reverse("awesome")); // 'emosewa'
-console.log(reverse("rithmschool")); // 'loohcsmhtir'
