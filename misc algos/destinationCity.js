bc1360ee92609b76617818da8e049ef728b797fa
Gopule <altusszawlowski@gmail.com>
2021-11-09 14:32:20 -0500

    destination city refactored

--- misc algos/destinationCity.js
+++ misc algos/destinationCity.js
@@ -1,14 +1,29 @@
 var destCity = function (paths) {
-  let locations = {};
-  for (let cv of paths) {
-    if (!locations[cv[0]]) locations[cv[0]] = 1;
-    if (locations[cv[0]]) locations[cv[0]]++;
-    if (!locations[cv[1]]) locations[cv[1]] = 1;
+  let result;
+  const hash = {};
+  for (let i = 0; i < paths.length; i++) {
+    hash[paths[i][0]] = true;
   }
-  for (let key in locations) {
-    if (locations[key] === 1) return key;
+  for (let i = 0; i < paths.length; i++) {
+    if (!hash[paths[i][1]]) {
+      result = paths[i][1];
+      break;
+    }
   }
+  return result;
 };
+
+// var destCity = function (paths) {
+//   let locations = {};
+//   for (let cv of paths) {
+//     if (!locations[cv[0]]) locations[cv[0]] = 1;
+//     if (locations[cv[0]]) locations[cv[0]]++;
+//     if (!locations[cv[1]]) locations[cv[1]] = 1;
+//   }
+//   for (let key in locations) {
+//     if (locations[key] === 1) return key;
+//   }
+// };
 
 console.log(
   destCity([
