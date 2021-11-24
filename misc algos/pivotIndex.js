252dce7b8c5e97d265f5b30b2b9511a1cbbcfa1a
Gopule <altusszawlowski@gmail.com>
2021-11-24 12:36:15 -0500

    pivot index refactored

--- misc algos/pivotIndex.js
+++ misc algos/pivotIndex.js
@@ -1,18 +1,31 @@
 function pivotIndex(nums) {
   let left = 0;
   let right = 0;
-  let index = 0;
   for (let i = 1; i < nums.length; i++) {
     right += nums[i];
   }
-  while (index < nums.length) {
-    if (right === left) return index;
-    left += nums[index];
-    right -= nums[index + 1];
-    index++;
+  for (let i = 0; i < nums.length; i++) {
+    if (left === right) return i;
+    left += nums[i];
+    right -= nums[i + 1];
   }
   return -1;
 }
+// function pivotIndex(nums) {
+//   let left = 0;
+//   let right = 0;
+//   let index = 0;
+//   for (let i = 1; i < nums.length; i++) {
+//     right += nums[i];
+//   }
+//   while (index < nums.length) {
+//     if (right === left) return index;
+//     left += nums[index];
+//     right -= nums[index + 1];
+//     index++;
+//   }
+//   return -1;
+// }
 
 console.log(pivotIndex([1, 7, 3, 6, 5, 6])); //3
 console.log(pivotIndex([1, 2, 3])); //-1
