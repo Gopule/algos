53ceea580575c211f7d4e7ee36b05678e31f0356
Gopule <altusszawlowski@gmail.com>
2021-12-01 21:02:45 -0500

    keyboard rows

--- memo-frequency/keyboardRows.js
+++ memo-frequency/keyboardRows.js
@@ -1,3 +1,11 @@
+// Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+
+// In the American keyboard:
+
+// the first row consists of the characters "qwertyuiop",
+// the second row consists of the characters "asdfghjkl", and
+// the third row consists of the characters "zxcvbnm".
+
 var findWords = function (words) {
   const row1 = new Set("qwertyuiop");
   const row2 = new Set("asdfghjkl");
