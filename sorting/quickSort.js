aa7c3d43026c61bd54ed99b2ca5b84659148f5fd
Gopule <altusszawlowski@gmail.com>
2021-12-20 01:26:07 -0500

    quickSort refactor

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -1,10 +1,10 @@
-function pivot(arr, start = 0, end = arr.length - 1) {
+const pivot = (arr, start = 0, end = arr.length - 1) => {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
 
   // We are assuming the pivot is always the first element
-  let pivot = arr[start];
+  const pivot = arr[start];
   let swapIdx = start;
 
   for (let i = start + 1; i <= end; i++) {
@@ -17,17 +17,17 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   // Swap the pivot from the start the swapPoint
   swap(arr, start, swapIdx);
   return swapIdx;
-}
+};
 
-function quickSort(arr, left = 0, right = arr.length - 1) {
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
   if (left < right) {
-    let pivotIndex = pivot(arr, left, right); //3
+    const pivotIndex = pivot(arr, left, right); //3
     //left
     quickSort(arr, left, pivotIndex - 1);
     //right
     quickSort(arr, pivotIndex + 1, right);
   }
   return arr;
-}
+};
 
 console.log(quickSort([4, 8, 6, 2, 5, 3, 1, 7]));
