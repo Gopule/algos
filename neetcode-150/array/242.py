96d0f3022a71f9dc421a4e64bd60a6e70802ceb7
Gopule <altusszawlowski@gmail.com>
2023-10-25 21:27:45 -0500

    refactor

--- neetcode-150/array/242.py
+++ neetcode-150/array/242.py
@@ -1,6 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
+        if len(s) != len(t):
+            return False
         s_map = Counter(s)
         for char in t:
             if char not in s_map or s_map[char] == 0:
