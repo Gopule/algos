7ec2fac77d88f1f64537d35c28ebfe1ecf195740
Gopule <altusszawlowski@gmail.com>
2023-11-21 21:22:17 -0600

    stack

---
+++ neetcode-150/stack/20.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        parens = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+        for char in s:
+            if char in parens and len(stack) > 0 and parens[char] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(char)
+        return len(stack) == 0
