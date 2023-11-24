b047e676ffda6bc96b49b5b980a00e9012fa1a76
Gopule <altusszawlowski@gmail.com>
2023-11-24 12:51:32 -0600

    stack

---
+++ neetcode-150/stack/71.py
@@ -0,0 +1,29 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = ["/"]
+        ops = set(["..", ".", ""])
+        dir_str = ""
+        for i in range(1, len(path)):
+            if path[i] == "/":
+                if dir_str in ops:
+                    if dir_str == "" or dir_str == ".":
+                        dir_str = ""
+                        continue
+                    else:
+                        if len(stack) > 1:
+                            stack.pop()
+                else:
+                    dir_str = "/" + dir_str if len(stack) > 1 else dir_str
+                    stack.append(dir_str)
+                dir_str = ""
+            else:
+               dir_str += path[i]
+
+        if dir_str == "..":
+            if len(stack) > 1:
+                stack.pop()
+        elif dir_str not in ops:
+            dir_str = "/" + dir_str if len(stack) > 1 else dir_str
+            stack.append(dir_str)
+
+        return "".join(stack)
