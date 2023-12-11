0bf65f25336f33643946f45422ff8be4fc585957
Gopule <altusszawlowski@gmail.com>
2023-12-11 16:17:22 -0600

    tortoise and hare refactor

--- neetcode-all/linked-list/141.py
+++ neetcode-all/linked-list/141.py
@@ -1,8 +1,19 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        while head:
-            if head.val is None:
+        if not head:
+            return False
+        slow, fast = head, head.next
+        while fast and fast.next:
+            if slow is fast:
                 return True
-            head.val = None
-            head = head.next
+            slow = slow.next
+            fast = fast.next.next
         return False
+
+    # def hasCycle(self, head: Optional[ListNode]) -> bool:
+    #     while head:
+    #         if head.val is None:
+    #             return True
+    #         head.val = None
+    #         head = head.next
+    #     return False
