f569512ade09b640ba4570b7ba3ea770b845d1a9
Gopule <altusszawlowski@gmail.com>
2024-01-07 14:35:56 -0600

    backtracking

---
+++ neetcode-all/backtracking/39.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        def dfs(pos, target, sub):
+            if target == 0:
+                res.append(sub.copy())
+            if target < 0:
+                return
+            for i in range(pos, len(candidates)):
+                sub.append(candidates[i])
+                dfs(i, target - candidates[i], sub)
+                sub.pop()
+
+        dfs(0, target, [])
+        return res
